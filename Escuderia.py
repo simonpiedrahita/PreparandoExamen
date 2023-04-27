279668ade5370cac7b000758fd9367d9e4a78aad
dcm323 <danielcalle323@gmail.com>
2023-04-27 06:54:50 -0500

    NT10 se crea clase Escuderia

---
+++ Escuderia.py
@@ -0,0 +1,9 @@
+from Piloto import Piloto
+class Escuderia:
+    def __init__(self):
+        self.__nombre=None
+        self.__motor= None
+        self.__costoAnual= None
+        self.__piloto1=Piloto()
+        self.__piloto2=Piloto()
+        
