35da01e795e37608db1e4a7eb32f0efae49a3f63
simonpiedrahita <simonpiedrahita@gmail.com>
2023-04-27 08:10:41 -0500

    se agrega la clase main y se implementael ciclo de llenado de escuderias

---
+++ main.py
@@ -0,0 +1,20 @@
+from Escuderia import Escuderia 
+
+escuderias=[]
+
+numeroEscuderias=0
+sumatoriaDeSalarios=0
+while(numeroEscuderias <= 2):
+    escuderia = Escuderia()
+    escuderia.nombre=input("Digita el nombre de la escuderia")
+    escuderia.motor=input("digitel el motor de la escuderia ")
+    escuderia.costoAnual=int(input("digite el costo anual"))
+    #escuderia.piloto1.nombre=input("digite el nombre del piloto")
+    #escuderia.piloto2.nombre=input("digite el nombre del piloto")
+    escuderia.piloto1.salarioAnual=int(input("digital el salario del pilot1"))
+    escuderia.piloto2.salarioAnual=int(input("digital el salario del pilot2"))
+    escuderias.append(escuderia)
+    numeroEscuderias+=1
+
+for escuderia in escuderias:
+    print(f"{escuderia.nombre}")
