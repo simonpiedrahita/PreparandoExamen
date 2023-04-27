1d24775f9bf761a223f67bc5752f54155b084634
dcm323 <danielcalle323@gmail.com>
2023-04-27 06:41:10 -0500

    nt11 Se termina trabajo de Pablo por vacaciones forzosas

--- Piloto.py
+++ Piloto.py
@@ -15,4 +15,34 @@ class Piloto:
     def nombre(self, dato):
         self.__nombre = dato
         
+    @property 
+    def apellido(self):
+        return self.__apellido
+    
+    @apellido.setter
+    def apellido(self, dato):
+        self.__apellido = dato
+    
+    @property 
+    def fechaNacimiento(self):
+        return self.__fechaNacimiento
+    
+    @fechaNacimiento.setter
+    def fechaNacimiento(self, dato):
+        self.__fechaNacimiento = dato
+
+    @property 
+    def pais(self):
+        return self.__pais
+    
+    @pais.setter
+    def pais(self, dato):
+        self.__pais = dato
         
+    @property 
+    def salarioAnual(self):
+        return self.__salarioAnual
+    
+    @salarioAnual.setter
+    def salarioAnual(self, dato):
+        self.__salarioAnual = dato
