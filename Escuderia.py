b46a2b4135f27db5c4d7403cb250b95612a07d34
dcm323 <danielcalle323@gmail.com>
2023-04-27 07:03:15 -0500

    Se crean getter and setters de la clase escuderia

--- Escuderia.py
+++ Escuderia.py
@@ -6,4 +6,43 @@ class Escuderia:
         self.__costoAnual= None
         self.__piloto1=Piloto()
         self.__piloto2=Piloto()
-        
+    
+    @property 
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, dato):
+        self.__nombre = dato
+
+    @property 
+    def motor(self):
+        return self.__motor
+    
+    @motor.setter
+    def motor(self, dato):
+        self.__motor = dato
+
+    @property 
+    def costoAnual(self):
+        return self.__costoAnual
+    
+    @costoAnual.setter
+    def costoAnual(self, dato):
+        self.__costoAnual = dato
+
+    @property 
+    def piloto1(self):
+        return self.__piloto1
+    
+    @piloto1.setter
+    def piloto1(self, dato):
+        self.__piloto1 = dato
+
+    @property 
+    def piloto2(self):
+        return self.__piloto2
+    
+    @piloto2.setter
+    def piloto2(self, dato):
+        self.__piloto2 = dato
